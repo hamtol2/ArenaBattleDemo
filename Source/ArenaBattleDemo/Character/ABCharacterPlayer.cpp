19e8ef30061e95f44f7d3edc332f8e5c245ace25
ronniej <ronniej@naver.com>
2025-04-11 09:53:46 +0900

    IA_Move 입력 액션에 Swizzle Modifier 적용 및 Move 함수 변경

--- Source/ArenaBattleDemo/Character/ABCharacterPlayer.cpp
+++ Source/ArenaBattleDemo/Character/ABCharacterPlayer.cpp
@@ -120,8 +120,8 @@ void AABCharacterPlayer::Move(const FInputActionValue& Value)
 	FVector RightVector = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
 
 	// 무브먼트 컴포넌트에 값 전달.
-	AddMovementInput(ForwardVector, Movement.Y);
-	AddMovementInput(RightVector, Movement.X);
+	AddMovementInput(ForwardVector, Movement.X);
+	AddMovementInput(RightVector, Movement.Y);
 }
 
 void AABCharacterPlayer::Look(const FInputActionValue& Value)
