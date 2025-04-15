18d1beecb597e0ce9d8313fd315d9b4b8d400aed
ronniej <ronniej@naver.com>
2025-04-15 12:17:00 +0900

    콤보 공격 적용

--- Source/ArenaBattleDemo/Character/ABCharacterPlayer.cpp
+++ Source/ArenaBattleDemo/Character/ABCharacterPlayer.cpp
@@ -88,6 +88,12 @@ AABCharacterPlayer::AABCharacterPlayer()
 		ChangeControlAction = ChangeControlActionRef.Object;
 	}
 
+	static ConstructorHelpers::FObjectFinder<UInputAction> AttackActionRef(TEXT("/Game/ArenaBattle/Input/Actions/IA_Attack.IA_Attack"));
+	if (AttackActionRef.Object)
+	{
+		AttackAction = AttackActionRef.Object;
+	}
+
 	// 초기 설정.
 	CurrentCharacterControlType = ECharacterControlType::Quarter;
 }
@@ -109,6 +115,9 @@ void AABCharacterPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputC
 	// Binding.
 	EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ACharacter::Jump);
 	EnhancedInputComponent->BindAction(ChangeControlAction, ETriggerEvent::Triggered, this, &AABCharacterPlayer::ChangeCharacterControl);
+	EnhancedInputComponent->BindAction(AttackAction, ETriggerEvent::Triggered, this, &AABCharacterPlayer::Attack);
+	
+	
 	EnhancedInputComponent->BindAction(ShoulderMoveAction, ETriggerEvent::Triggered, this, &AABCharacterPlayer::ShoulderMove);
 	EnhancedInputComponent->BindAction(ShoulderLookAction, ETriggerEvent::Triggered, this, &AABCharacterPlayer::ShoulderLook);
 	EnhancedInputComponent->BindAction(QuarterMoveAction, ETriggerEvent::Triggered, this, &AABCharacterPlayer::QuarterMove);
@@ -195,6 +204,12 @@ void AABCharacterPlayer::QuarterMove(const FInputActionValue& Value)
 	
 	// 입력에 따른 방향으로 이동하도록 입력 전달.
 	AddMovementInput(MoveDirection, MovementVectorSize);
+}
+
+void AABCharacterPlayer::Attack()
+{
+	// 공격 입력 처리 함수 호출.
+	ProcessComboCommand();
 }
 
 void AABCharacterPlayer::ShoulderMove(const FInputActionValue& Value)
