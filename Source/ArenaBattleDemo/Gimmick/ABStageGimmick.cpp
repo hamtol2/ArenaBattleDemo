2c356fc67c46fe624de286c482109c12e5ecbcae
ronniej <ronniej@naver.com>
2025-04-22 12:07:21 +0900

    에디터에서 테스트가 가능하도록 OnConstruction 함수 처리 로직 추가

--- Source/ArenaBattleDemo/Gimmick/ABStageGimmick.cpp
+++ Source/ArenaBattleDemo/Gimmick/ABStageGimmick.cpp
@@ -39,7 +39,7 @@ AABStageGimmick::AABStageGimmick()
 	};
 
 	// 리소스 로드.
-	static ConstructorHelpers::FObjectFinder<UStaticMesh> GateMeshRef(TEXT(""));
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> GateMeshRef(TEXT("/Game/ArenaBattle/Environment/Props/SM_GATE.SM_GATE"));
 
 	// 게이트 생성.
 	for (const FName& GateSocket : GateSockets)
@@ -62,14 +62,149 @@ AABStageGimmick::AABStageGimmick()
 		Gates.Add(GateSocket, Gate);
 
 		// 문마다 배치할 BoxComponent 생성 및 설정.
+		FName TriggerName = *GateSocket.ToString().Append("Trigger");
+
+		// 컴포넌트 생성.
+		UBoxComponent* GateTrigger = CreateDefaultSubobject<UBoxComponent>(TriggerName);
+
+		// 컴포넌트 설정.
+		GateTrigger->SetBoxExtent(FVector(100.0f, 100.0f, 300.0f));
+		GateTrigger->SetupAttachment(Stage, GateSocket);
+		GateTrigger->SetRelativeLocation(FVector(70.0f, 0.0f, 250.0f));
+		GateTrigger->SetCollisionProfileName(CPROFILE_ABTRIGGER);
+		GateTrigger->OnComponentBeginOverlap.AddDynamic(
+			this,
+			&AABStageGimmick::OnGateTriggerBeginOverlap
+		);
+
+		// 태그 설정.
+		GateTrigger->ComponentTags.Add(GateSocket);
+
+		// 생성한 컴포넌트를 배열에 추가.
+		GateTriggers.Add(GateTrigger);
 	}
 
+	// 시작할 때는 준비 상태로 설정.
+	CurrentState = EStageState::Ready;
+
+	// 열거형 - 델리게이트 맵 설정.
+	StageChangedActions.Add(EStageState::Ready, FOnStageChangedDelegate::CreateUObject(this, &AABStageGimmick::SetReady));
+
+	StageChangedActions.Add(EStageState::Fight, FOnStageChangedDelegate::CreateUObject(this, &AABStageGimmick::SetFight));
+
+	StageChangedActions.Add(EStageState::Reward, FOnStageChangedDelegate::CreateUObject(this, &AABStageGimmick::SetChooseReward));
+
+	StageChangedActions.Add(EStageState::Next, FOnStageChangedDelegate::CreateUObject(this, &AABStageGimmick::SetChooseNext));
+}
+
+void AABStageGimmick::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	// OnConstruction이 호출될 때 상태 값도 갱신되도록 처리.
+	SetState(CurrentState);
+}
+
+void AABStageGimmick::SetState(EStageState InNewState)
+{
+	// 예외 처리.
+	//if (CurrentState == InNewState)
+	//{
+	//	return;
+	//}
+
+	// 현재 상태 업데이트.
+	CurrentState = InNewState;
+
+	// 전달된 상태에 맵에 포함돼 있으면 델리게이트 실행.
+	if (StageChangedActions.Contains(InNewState))
+	{
+		StageChangedActions[CurrentState].StageChangedDelegate.ExecuteIfBound();
+	}
 }
 
 void AABStageGimmick::OnStageTriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 }
 
+void AABStageGimmick::SetReady()
+{
+	// 가운데 트리거 활성화.
+	StageTrigger->SetCollisionProfileName(CPROFILE_ABTRIGGER);
+
+	// 플레이어가 게이트와 상호작용하지 않도록 콜리전 끄기.
+	for (const auto& GateTrigger : GateTriggers)
+	{
+		GateTrigger->SetCollisionProfileName(TEXT("NoCollision"));
+	}
+
+	// 모든 문 닫기.
+	CloseAllGates();
+}
+
+void AABStageGimmick::SetFight()
+{
+	// 가운데 트리거 활성화.
+	StageTrigger->SetCollisionProfileName(TEXT("NoCollision"));
+
+	// 플레이어가 게이트와 상호작용하지 않도록 콜리전 끄기.
+	for (const auto& GateTrigger : GateTriggers)
+	{
+		GateTrigger->SetCollisionProfileName(TEXT("NoCollision"));
+	}
+
+	// 모든 문 닫기.
+	CloseAllGates();
+}
+
+void AABStageGimmick::SetChooseReward()
+{
+	// 가운데 트리거 활성화.
+	StageTrigger->SetCollisionProfileName(TEXT("NoCollision"));
+
+	// 플레이어가 게이트와 상호작용하지 않도록 콜리전 끄기.
+	for (const auto& GateTrigger : GateTriggers)
+	{
+		GateTrigger->SetCollisionProfileName(TEXT("NoCollision"));
+	}
+
+	// 모든 문 닫기.
+	CloseAllGates();
+}
+
+void AABStageGimmick::SetChooseNext()
+{
+	// 가운데 트리거 활성화.
+	StageTrigger->SetCollisionProfileName(TEXT("NoCollision"));
+
+	// 플레이어가 게이트와 상호작용하지 않도록 콜리전 끄기.
+	for (const auto& GateTrigger : GateTriggers)
+	{
+		GateTrigger->SetCollisionProfileName(CPROFILE_ABTRIGGER);
+	}
+
+	// 모든 문 열기 (다른 스테이지로 이동할 수 있도록).
+	OpenAllGates();
+}
+
 void AABStageGimmick::OnGateTriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+}
+
+void AABStageGimmick::OpenAllGates()
+{
+	// 문 액터의 배열을 순회하면서 회전 설정.
+	for (const auto& Gate : Gates)
+	{
+		Gate.Value->SetRelativeRotation(FRotator(0.0f, -90.0f, 0.0f));
+	}
+}
+
+void AABStageGimmick::CloseAllGates()
+{
+	// 문 액터의 배열을 순회하면서 회전 설정.
+	for (const auto& Gate : Gates)
+	{
+		Gate.Value->SetRelativeRotation(FRotator::ZeroRotator);
+	}
 }
--- Source/ArenaBattleDemo/Gimmick/ABStageGimmick.h
+++ Source/ArenaBattleDemo/Gimmick/ABStageGimmick.h
@@ -6,6 +6,32 @@
 #include "GameFramework/Actor.h"
 #include "ABStageGimmick.generated.h"
 
+// 상태에 따른 처리를 위한 델리게이트 선언.
+DECLARE_DELEGATE(FOnStageChangedDelegate);
+
+// 래퍼 구조체.
+USTRUCT(BlueprintType)
+struct FOnStageChangedDelegateWrapper
+{
+	GENERATED_BODY()
+
+	FOnStageChangedDelegateWrapper() {}
+	FOnStageChangedDelegateWrapper(FOnStageChangedDelegate InDelegate)
+	: StageChangedDelegate(InDelegate) {}
+
+	FOnStageChangedDelegate StageChangedDelegate;
+};
+
+// 상태를 나타내는 열거형.
+UENUM(BlueprintType)
+enum class EStageState : uint8
+{
+	Ready = 0,
+	Fight,
+	Reward,
+	Next
+};
+
 UCLASS()
 class ARENABATTLEDEMO_API AABStageGimmick : public AActor
 {
@@ -14,6 +40,9 @@ class ARENABATTLEDEMO_API AABStageGimmick : public AActor
 public:
 	// Sets default values for this actor's properties
 	AABStageGimmick();
+
+	// 이 함수는 트랜스폼 뿐만 아니라, 다른 속성이 변경되는 경우에도 호출됨.
+	virtual void OnConstruction(const FTransform& Transform) override;
 
 	// Stage Section.
 protected:
@@ -24,7 +53,15 @@ protected:
 	UPROPERTY(VisibleAnywhere, Category = Stage, meta = (AllowPrivateAccess = "true"))
 	TObjectPtr<class UBoxComponent> StageTrigger;
 
+	// 현재 상태를 나타내는 열거형 변수.
+	UPROPERTY(EditAnywhere, Category = Stage, meta = (AllowPrivateAccess = "true"))
+	EStageState CurrentState;
+
+	// 상태를 설정할 때 사용할 함수.
+	void SetState(EStageState InNewState);
+
 	// 스테이지 트리거의 오버랩 이벤트 확인을 위한 함수.
+	UFUNCTION()
 	void OnStageTriggerBeginOverlap(
 		UPrimitiveComponent* OverlappedComponent, 
 		AActor* OtherActor, 
@@ -33,6 +70,16 @@ protected:
 		bool bFromSweep, 
 		const FHitResult& SweepResult
 	);
+
+	// 상태에 따른 처리를 위해 열거형과 델리게이트를 맵으로 관리.
+	UPROPERTY()
+	TMap<EStageState, FOnStageChangedDelegateWrapper> StageChangedActions;
+
+	// 상태 값에 따라 실행할 함수.
+	void SetReady();
+	void SetFight();
+	void SetChooseReward();
+	void SetChooseNext();
 
 	// Gate Section (4개의 문의 대한 섹션).
 protected:
@@ -45,6 +92,7 @@ protected:
 	TArray<TObjectPtr<class UBoxComponent>> GateTriggers;
 
 	// 게이트 트리거의 오버랩 이벤트 확인을 위한 함수.
+	UFUNCTION()
 	void OnGateTriggerBeginOverlap(
 		UPrimitiveComponent* OverlappedComponent,
 		AActor* OtherActor,
@@ -53,4 +101,8 @@ protected:
 		bool bFromSweep,
 		const FHitResult& SweepResult
 	);
+
+	// 문 열고/닫는 함수.
+	void OpenAllGates();
+	void CloseAllGates();
 };
