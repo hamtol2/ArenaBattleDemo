c1505e8f876547340de0e356bf4038e56f8a13b5
ronniej <ronniej@naver.com>
2025-04-28 15:46:31 +0900

    BT 로직 구현\

--- Source/ArenaBattleDemo/Character/ABCharacterNonPlayer.cpp
+++ Source/ArenaBattleDemo/Character/ABCharacterNonPlayer.cpp
@@ -105,7 +105,7 @@ float AABCharacterNonPlayer::GetAIAttackRange()
 
 float AABCharacterNonPlayer::GetAITurnSpeed()
 {
-	return 0.0f;
+	return 2.0f;
 }
 
 void AABCharacterNonPlayer::SetAIAttackDelegeate(const FAICharacterAttackFinished& InOnAttackFinished)
